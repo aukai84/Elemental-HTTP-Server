bf7c21dbacd69baa2d86e8ba3620466ca88da969
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-31 16:13:39 -1000

    started working on post..

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const PORT = process.env.PORT || 3000;
 const fs = require('fs');
 
 let resourceMapping = {
-  '/': '/public/index.html',
-  '/hydrogen.html': '/public/hydrogen.html',
-  '/helium.html': '/public/helium.html',
-  '/404.html': '/public/404.html',
-  '/css/styles.css': '/public/css/styles.css',
+  '/': './public/index.html',
+  '/hydrogen.html': './public/hydrogen.html',
+  '/helium.html': './public/helium.html',
+  '/index.html': './public/index.html',
+  '/404.html': './public/404.html',
+  '/css/styles.css': './public/css/styles.css',
   '/elements': '??'
 };
 
 function serverErrorHandler(res) {
   res.statusCode = 404;
-  res.setHeader("content-Type", 'text/plain');
+  res.setHeader("content-Type", 'text/html');
   res.write("Could not find content...");
   res.end();
 }
@@ -26,3 +27,29 @@ function sendContent(res, content) {
   res.write(content);
   res.end('connection closed');
 }
+
+let server = http.createServer( (req, res) => {
+
+  let reqBody = '';
+  req.setEncoding('utf8');
+  req.on('data', (chunk) => {
+    reqBody += chunk;
+  });
+  console.log(reqBody);
+  req.on('end', () => {
+
+    console.log(reqBody);
+    fs.readFile(resourceMapping[req.url] || '', (err, content) => {
+      if(err){
+        serverErrorHandler(res);
+        return;
+      }
+        sendContent(res, content);
+    });
+  });
+
+});
+
+server.listen(PORT, () => {
+  console.log('server is listening on port:' + PORT);
+});
